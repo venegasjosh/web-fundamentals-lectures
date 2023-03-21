d59a4e4981b3d1ac0a894c6b6a1191c0f774d2a2
venegasjosh <venegasjosh@gmail.com>
2023-03-20 22:49:51 -0500

    Monday Commit

---
+++ W2D1/More_DOM/script.js
@@ -0,0 +1,53 @@
+
+function changeTitle(){
+  //this one creates a variable and selects it
+  var h3 = document.querySelector("h3");
+
+//this line changes it
+  h3.innerText = "New Title";
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function chooseLunch(element) {
+  alert("You picked " + element.value);
+}
+
+
+
+
+
+var nameTag = document.querySelector("#name-tag");
+function setName(element) {
+    console.log(element.value);
+    nameTag.innerText = element.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
